80b3830d9a05254956b152b5012a11d15e7665b4
Ghastly2001 <divyanshverma18may@gmail.com>
2024-08-31 01:28:11 +0530

    contract-reader

--- Context/constants.js
+++ Context/constants.js
@@ -14,4 +14,22 @@ const REWARD_TOKEN = process.env.NEXT_PUBLIC_REWARD_TOKEN;
 //convert amount to eths
 export function toEth(amount, decimals) {
   const toEth = ethers.utils.formatUnits(amount, decimals);
+  return toEth.toString();
 }
+
+export const tokenContract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+
+  if (ethereum) {
+    const signer = provider.getSigner();
+
+    const contractReader = new ethers.Contract(
+      DEPOSIT_TOKEN,
+      CustomTokenABI,
+      signer
+    );
+
+    return contractReader;
+  }
+};
